4382f2f7eeaa0480186fe2d8991a6c8e2455ee07
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2018-02-06 20:40:16 -0600

    Switch to sucker_punch instead of workers. Also added rescheduling of ratelimited replies.

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ $config[:reddit] ||= {}
 $config[:reddit][:user_agent] ||= 'Substitute-Bot'
 $config[:reddit][:client_id] ||= ENV['REDDIT_CLIENT_ID']
 $config[:reddit][:client_secret] ||= ENV['REDDIT_CLIENT_SECRET']
-$config[:reddit][:bot_username] ||= ENV['REDDIT_BOT_USERNAME']
+$config[:reddit][:bot_username] ||= ENV['REDDIT_BOT_USERNAME'].downcase
 $config[:reddit][:bot_password] ||= ENV['REDDIT_BOT_PASSWORD']
 
 def bot_config
--- substitute_worker.rb
+++ substitute_worker.rb
@@ -1,36 +1,60 @@
-require 'workers'
 require 'redd'
+require 'sucker_punch'
 require_relative 'config'
 
-class SubstituteWorker < Workers::Worker
-  private
+class SubstituteWorker
+  include SuckerPunch::Job
+  workers 4
 
-  def event_handler(event)
-    case event.command
-    when :new_comment
-      handle_new_comment event.data
-    else
-      super(event)
+  def perform(comment_id, comment_body)
+    begin
+      handle_new_comment(comment_id, comment_body)
+    rescue Redd::Errors::APIError => e
+      error = e.response.body[:json][:errors][0]
+      raise unless error[0].downcase == 'ratelimit'
+      reschedule_after_ratelimit(comment_id, comment_body, error[1])
     end
   end
 
-  def reddit_session
-    @reddit_session ||= Redd.it(reddit_session_params)
+  def reschedule_after_ratelimit(comment_id, comment_body, message)
+    time = message.match(/\b(\d+)\s+(seconds|minutes|hours)/i).captures
+    seconds = Integer(time[0])
+    seconds *= 60 if time[1].downcase.start_with?('minute')
+    seconds *= 60 * 60 if time[1].downcase.start_with?('hour')
+    seconds += 5 # Add 5 seconds for good measure
+
+    puts "Hit ratelimit error. Rescheduling to run in #{seconds} seconds. id: #{comment_id}"
+
+    SubstituteWorker::perform_in(seconds, comment_id, comment_body)
   end
 
-  def handle_new_comment(data)
-    command = self.class.scan_for_substitute_command(data[:body])
+  private
+
+  @@reddit_session = Concurrent::ThreadLocalVar.new(nil)
+
+  def reddit_session
+    @@reddit_session.value ||= Redd.it(reddit_session_params)
+  end
+
+  def handle_new_comment(comment_id, comment_body)
+    command = self.class.scan_for_substitute_command(comment_body)
     return unless command
 
-    comment = reddit_session.from_ids(data[:id]).first
+    comment = reddit_session.from_ids(comment_id).first
+
+
     parent = comment.parent
     return unless parent.is_a?(Redd::Models::Comment)
 
-    substituted = self.class.substitute(parent.body, command[0], "\*\*#{command[1]}\*\*")
+    # Don't respond to self comments
+    return if reddit_session_params[:username] == comment.author.name.downcase
+    return if reddit_session_params[:username] == parent.author.name.downcase
+
+    substituted = self.class.substitute(parent.body, command[0], "**#{command[1]}**")
     return unless substituted
 
     reply = comment.reply(substituted +
-                          "\n\n This was posted by a bot. Upvote me if you like what I did. [Source](#{bot_config[:source_url]})".gsub(/ /, ' ^^'))
+                        "\n\n This was posted by a bot. Upvote me if you like what I did. [Source](#{bot_config[:source_url]})".gsub(/ /, ' ^^'))
     puts "Posted reply. id: #{reply.name}"
   end
 
