aeac7a1dd9ecc96890519630a71a16b8440eed26
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2018-02-09 23:42:47 -0600

    Add trivial index that shows summary & recent posts

--- README.md
+++ README.md
@@ -9,9 +9,15 @@ A [Reddit](https://www.reddit.com/) bot that provides the ability to search and 
 - `ruby bot.rb`
 
 ## Gems
+### Bot
 - sucker_punch (for threaded handling of comments)
 - Redd (Reddit API wrapper)
 - RSpec (for tests)
+- redis-namespace
+
+### Web
+- Sinatra
+- Haml
 
 ## Testing
 - `bundle exec rspec`
--- web.rb
+++ web.rb
@@ -1,14 +1,16 @@
 require 'sinatra'
 require 'trinidad'
+require 'redd'
 
 require_relative 'globals'
+require_relative 'reply_store'
 
 configure do
   set :server, :trinidad
 end
 
 get '/' do
-  haml :index, locals: { bingo: 'bango' }
+  haml :index, locals: { replies: Redd.it(reddit_session_params).from_ids(ReplyStore::get_replies) }
 end
 
 get '/stylesheets/style.css' do
