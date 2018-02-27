323d1e28fefe0a88ce6552203abb3e6f35dc18bd
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2018-02-26 18:49:15 -0600

    Add boxes around recent replies

--- web.rb
+++ web.rb
@@ -6,6 +6,8 @@ require_relative 'reply_store'
 
 configure do
   set :server, :trinidad
+  set :show_exceptions, false
+  set :raise_errors, false
 end
 
 get '/' do
@@ -14,4 +16,8 @@ end
 
 get '/stylesheets/style.css' do
   scss :style
+end
+
+not_found do
+  redirect to('/')
 end
