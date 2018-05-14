533ae22811ccbd0fed8b3349118642f41bdacd3d
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2018-05-13 22:22:16 -0500

    Add retry on errors after successful initialization & a heartbeat console message

--- bot.rb
+++ bot.rb
@@ -3,10 +3,33 @@ require_relative 'comment_processor'
 
 require 'redd'
 
-def run
-  Redd.it(reddit_session_params).subreddit('all').comments.stream do |comment|
-    CommentProcessor::perform_async(comment.name, comment.body)
+class Heartbeat
+  def initialize(beat_interval_in_seconds = 5 * 60)
+    @last_beat = Time.now
+    @interval = beat_interval_in_seconds
+  end
+
+  def beat
+    now = Time.now
+    if now - @last_beat > @interval
+      @last_beat = now
+      puts 'Heartbeat...'
+    end
   end
 end
 
-run
+def run(client = Redd.it(reddit_session_params))
+  heartbeat = Heartbeat.new
+  client.subreddit('all').comments.stream do |comment|
+    CommentProcessor::perform_async(comment.name, comment.body)
+    heartbeat.beat
+  end
+end
+
+client = Redd.it(reddit_session_params)
+begin
+  run(client)
+rescue => e
+  puts "#{e.class}: #{e.to_s}"
+  retry
+end
